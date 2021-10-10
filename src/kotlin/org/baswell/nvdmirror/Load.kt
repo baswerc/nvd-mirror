703432147c4e8406d0359407170d4ae7d7e448f6
cbaswell <corey.baswell@tekfive.com>
2021-10-10 07:16:14 -0500

    2021-10-10:07:16:14

--- src/kotlin/org/baswell/nvdmirror/Load.kt
+++ src/kotlin/org/baswell/nvdmirror/Load.kt
@@ -14,85 +14,22 @@ const val SleepBufferMSecs = 2000L
 
 fun main() {
 
-    while (true) {
-        if (downloadTextFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.meta")) {
-            break
-        } else {
-            Thread.sleep(SleepBufferMSecs)
-        }
-    }
-
-    while (true) {
-        if (downloadTextFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.meta")) {
-            break
-        } else {
-            Thread.sleep(SleepBufferMSecs)
-        }
-    }
-
+    downloadTextFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.meta")
+    downloadTextFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.meta")
     for (i in 2002..2021) {
-        var fileSaved = false
-        while (!fileSaved) {
-            if (downloadTextFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$i.meta")) {
-                break
-            } else {
-                Thread.sleep(SleepBufferMSecs)
-            }
-        }
+        downloadTextFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$i.meta")
     }
 
-    while (true) {
-        if (downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.json.gz")) {
-            break
-        } else {
-            Thread.sleep(SleepBufferMSecs)
-        }
-    }
-
-    while (true) {
-        if (downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.json.gz")) {
-            break
-        } else {
-            Thread.sleep(SleepBufferMSecs)
-        }
-    }
-
+    downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.json.gz")
+    downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.json.gz")
     for (i in 2002..2021) {
-        var fileSaved = false
-        while (!fileSaved) {
-            if (downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$i.json.gz")) {
-                break
-            } else {
-                Thread.sleep(SleepBufferMSecs)
-            }
-        }
+        downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$i.json.gz")
     }
 
-    while (true) {
-        if (downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.json.zip")) {
-            break
-        } else {
-            Thread.sleep(SleepBufferMSecs)
-        }
-    }
-
-    while (true) {
-        if (downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.json.zip")) {
-            break
-        } else {
-            Thread.sleep(SleepBufferMSecs)
-        }
-    }
-
+    downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.json.zip")
+    downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.json.zip")
     for (i in 2002..2021) {
-        var fileSaved = false
-        while (!fileSaved) {
-            if (downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$i.json.zip")) {
-                break
-            } else {
-                Thread.sleep(SleepBufferMSecs)
-            }
-        }
+        downloadBinaryFile("https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$i.json.zip")
     }
 
     val timestamp = SimpleDateFormat("YYYY-MM-dd:HH:mm:ss").format(Date())
@@ -117,37 +54,50 @@ fun main() {
     if (result != 0) {
         throw IOException("Git pushed failed with result $result")
     }
-
-
-
 }
 
-fun downloadTextFile(url: String): Boolean {
+fun downloadTextFile(url: String) {
     println(url)
-    val request: Request = Request.Builder().url(url).header("Accept-Encoding", "gzip, deflate").header("Connection", "keep-alive").build()
-    return client.newCall(request).execute().use { response ->
-        val code = response.code
-        if (code != 200) {
-            println(response.body!!.string())
-            false
+    while (true) {
+        val request: Request = Request.Builder().url(url).header("Accept-Encoding", "gzip, deflate").header("Connection", "keep-alive").build()
+        val fileSaved = client.newCall(request).execute().use { response ->
+            val code = response.code
+            if (code != 200) {
+                println(response.body!!.string())
+                false
+            } else {
+                FileOutputStream(urlToFileName(url)).use { it.write(GZIPInputStream(response.body!!.byteStream()).readAllBytes()) }
+                true
+            }
+        }
+
+        if (fileSaved) {
+            break
         } else {
-            FileOutputStream(urlToFileName(url)).use { it.write(GZIPInputStream(response.body!!.byteStream()).readAllBytes()) }
-            true
+            Thread.sleep(SleepBufferMSecs)
         }
     }
 }
 
-fun downloadBinaryFile(url: String): Boolean {
+fun downloadBinaryFile(url: String) {
     println(url)
-    val request: Request = Request.Builder().url(url).header("Connection", "keep-alive").build()
-    return client.newCall(request).execute().use { response ->
-        val code = response.code
-        if (code != 200) {
-            println(response.body!!.string())
-            false
+    while (true) {
+        val request: Request = Request.Builder().url(url).header("Connection", "keep-alive").build()
+        val fileSaved =  client.newCall(request).execute().use { response ->
+            val code = response.code
+            if (code != 200) {
+                println(response.body!!.string())
+                false
+            } else {
+                FileOutputStream(urlToFileName(url)).use { it.write(response.body!!.byteStream().readAllBytes()) }
+                true
+            }
+        }
+
+        if (fileSaved) {
+            break
         } else {
-            FileOutputStream(urlToFileName(url)).use { it.write(response.body!!.byteStream().readAllBytes()) }
-            true
+            Thread.sleep(SleepBufferMSecs)
         }
     }
 }
